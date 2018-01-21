9705f4ab2ca32b32c7f55023c2cdc5627f9127bb
kwang <kwang@gorillas-MacBook-Pro-7.local>
2018-01-20 19:13:42 -0500

    Fix carousel styling

--- src/app/components/card/card.js
+++ src/app/components/card/card.js
@@ -37,73 +37,168 @@ const Card = ({ player, stats, removePlayer, onImgLoad, isImgLoaded, onClick, is
                 </div>
             </div>
             <div className={ componentClass + '__stats' }>
-                <Carousel
-                    infiniteLoop
-                    emulateTouch
-                    showStatus={false}
-                    showIndicators={false}
-                    showThumbs={false}>
-                {!!stats && stats.slice(0).reverse().map((season, i) => {
-                    return (
-                        <div key={i}>
-                            <div className={ componentClass + '__data-title'}>
-                                <span>{season.team.name}</span>
-                            </div>
-                            <div className={ componentClass + '__data-container'}>
-                                <table className={ componentClass + '__data table' }>
+                {!!stats && stats.length > 1 &&
+                    <Carousel
+                        infiniteLoop
+                        emulateTouch
+                        showStatus={false}
+                        showIndicators={false}
+                        showThumbs={false}>
+                        {stats.slice(0).reverse().map((season, i) => {
+                            return (
+                                <div key={i} className={ componentClass + '__data-wrapper'}>
+                                    <div className={ componentClass + '__data-title'}>
+                                        <span>{season.team.name}</span>
+                                    </div>
+                                    <div className={ componentClass + '__data-container'}>
+                                        <table className={ componentClass + '__data table' }>
+                                            <thead>
+                                            {player.pos !== 'G' ?
+                                                <tr>
+                                                    <th>GP</th>
+                                                    <th>G</th>
+                                                    <th>A</th>
+                                                    <th>P</th>
+                                                    <th>+/-</th>
+                                                </tr>
+                                                :
+                                                <tr>
+                                                    <th>GP</th>
+                                                    <th>Record</th>
+                                                    <th>Sv%</th>
+                                                    <th>GAA</th>
+                                                    <th>SO</th>
+                                                </tr>
+                                            }
+                                            </thead>
+                                            <tbody>
+                                            {player.pos !== 'G' ?
+                                                <tr>
+                                                    <td>{typeof season.stat.games === 'undefined' ? '-' : season.stat.games}</td>
+                                                    <td>{typeof season.stat.goals === 'undefined' ? '-' : season.stat.goals}</td>
+                                                    <td>{typeof season.stat.assists === 'undefined' ? '-' : season.stat.assists}</td>
+                                                    <td>{typeof season.stat.points === 'undefined' ? '-' : season.stat.points}</td>
+                                                    <td>{typeof season.stat.plusMinus === 'undefined' ? '-' : season.stat.plusMinus}</td>
+                                                </tr>
+                                                :
+                                                <tr>
+                                                    <td>{typeof season.stat.games === 'undefined' ? '-' : season.stat.games}</td>
+                                                    <td>{typeof season.stat.wins === 'undefined' ? '-' : season.stat.wins + '-' + season.stat.losses + '-' + season.stat.ot}</td>
+                                                    <td>{typeof season.stat.savePercentage === 'undefined' ? '-' : season.stat.savePercentage}</td>
+                                                    <td>{typeof season.stat.goalAgainstAverage === 'undefined' ? '-' : season.stat.goalAgainstAverage}</td>
+                                                    <td>{typeof season.stat.shutouts === 'undefined' ? '-' : season.stat.shutouts}</td>
+                                                </tr>
+                                            }
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </Carousel>
+                }
+                {!!stats && stats.length === 1 &&
+                    <div className={ componentClass + '__data-wrapper'}>
+                        <div className={ componentClass + '__data-title'}>
+                            <span>{stats[0].team.name}</span>
+                        </div>
+                        <div className={ componentClass + '__data-container'}>
+                            <table className={ componentClass + '__data table' }>
                                 <thead>
-                                    {player.pos !== 'G' ?
-                                        <tr>
-                                            <th>GP</th>
-                                            <th>G</th>
-                                            <th>A</th>
-                                            <th>P</th>
-                                            <th>+/-</th>
-                                        </tr>
+                                {player.pos !== 'G' ?
+                                    <tr>
+                                        <th>GP</th>
+                                        <th>G</th>
+                                        <th>A</th>
+                                        <th>P</th>
+                                        <th>+/-</th>
+                                    </tr>
                                     :
-                                        <tr>
-                                            <th>GP</th>
-                                            <th>Record</th>
-                                            <th>Sv%</th>
-                                            <th>GAA</th>
-                                            <th>SO</th>
-                                        </tr>
-                                    }
+                                    <tr>
+                                        <th>GP</th>
+                                        <th>Record</th>
+                                        <th>Sv%</th>
+                                        <th>GAA</th>
+                                        <th>SO</th>
+                                    </tr>
+                                }
                                 </thead>
                                 <tbody>
-                                    {player.pos !== 'G' ?
-                                            <tr>
-                                                <td>{typeof season.stat.games === 'undefined' ?  '-' : season.stat.games}</td>
-                                                <td>{typeof season.stat.goals === 'undefined' ?  '-' : season.stat.goals}</td>
-                                                <td>{typeof season.stat.assists === 'undefined' ?  '-' : season.stat.assists}</td>
-                                                <td>{typeof season.stat.points === 'undefined' ?  '-' : season.stat.points}</td>
-                                                <td>{typeof season.stat.plusMinus === 'undefined' ?  '-' : season.stat.plusMinus}</td>
-                                            </tr>
-                                        :
-
-                                        <tr>
-                                            <td>{typeof season.stat.games === 'undefined' ?  '-' : season.stat.games}</td>
-                                            <td>{typeof season.stat.wins === 'undefined' ?  '-' : season.stat.wins + '-' + season.stat.losses + '-' + season.stat.ot}</td>
-                                            <td>{typeof season.stat.savePercentage === 'undefined' ?  '-' : season.stat.savePercentage}</td>
-                                            <td>{typeof season.stat.goalAgainstAverage === 'undefined' ?  '-' : season.stat.goalAgainstAverage}</td>
-                                            <td>{typeof season.stat.shutouts === 'undefined' ?  '-' : season.stat.shutouts}</td>
-                                        </tr>
-                                    }
+                                {player.pos !== 'G' ?
+                                    <tr>
+                                        <td>{typeof stats[0].stat.games === 'undefined' ? '-' : stats[0].stat.games}</td>
+                                        <td>{typeof stats[0].stat.goals === 'undefined' ? '-' : stats[0].stat.goals}</td>
+                                        <td>{typeof stats[0].stat.assists === 'undefined' ? '-' : stats[0].stat.assists}</td>
+                                        <td>{typeof stats[0].stat.points === 'undefined' ? '-' : stats[0].stat.points}</td>
+                                        <td>{typeof stats[0].stat.plusMinus === 'undefined' ? '-' : stats[0].stat.plusMinus}</td>
+                                    </tr>
+                                    :
+                                    <tr>
+                                        <td>{typeof stats[0].stat.games === 'undefined' ? '-' : stats[0].stat.games}</td>
+                                        <td>{typeof stats[0].stat.wins === 'undefined' ? '-' : stats[0].stat.wins + '-' + stats[0].stat.losses + '-' + stats[0].stat.ot}</td>
+                                        <td>{typeof stats[0].stat.savePercentage === 'undefined' ? '-' : stats[0].stat.savePercentage}</td>
+                                        <td>{typeof stats[0].stat.goalAgainstAverage === 'undefined' ? '-' : stats[0].stat.goalAgainstAverage}</td>
+                                        <td>{typeof stats[0].stat.shutouts === 'undefined' ? '-' : stats[0].stat.shutouts}</td>
+                                    </tr>
+                                }
                                 </tbody>
-                                </table>
-                            </div>
+                            </table>
                         </div>
-                    )
-                })}
-                </Carousel>
-
+                    </div>
+                }
+                {!!stats && !Object.keys(stats).length &&
+                    <div className={ componentClass + '__data-wrapper'}>
+                        <div className={ componentClass + '__data-title'}>
+                            <span>No stats available</span>
+                        </div>
+                        <div className={ componentClass + '__data-container'}>
+                            <table className={ componentClass + '__data table' }>
+                                <thead>
+                                {player.pos !== 'G' ?
+                                    <tr>
+                                        <th>GP</th>
+                                        <th>G</th>
+                                        <th>A</th>
+                                        <th>P</th>
+                                        <th>+/-</th>
+                                    </tr>
+                                    :
+                                    <tr>
+                                        <th>GP</th>
+                                        <th>Record</th>
+                                        <th>Sv%</th>
+                                        <th>GAA</th>
+                                        <th>SO</th>
+                                    </tr>
+                                }
+                                </thead>
+                                <tbody>
+                                {player.pos !== 'G' ?
+                                    <tr>
+                                        <td>-</td>
+                                        <td>-</td>
+                                        <td>-</td>
+                                        <td>-</td>
+                                        <td>-</td>
+                                    </tr>
+                                    :
+                                    <tr>
+                                        <td>-</td>
+                                        <td>-</td>
+                                        <td>-</td>
+                                        <td>-</td>
+                                        <td>-</td>
+                                    </tr>
+                                }
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                }
                 {!stats &&
                     <div className="c-loader p-4"></div>
                 }
 
-                {!!stats && !Object.keys(stats).length &&
-                    <span className={ componentClass + '__message d-block text-center mb-3'}>No Stats Available</span>
-                }
             </div>
         </div>
     </div>
